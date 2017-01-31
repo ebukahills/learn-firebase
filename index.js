04ddad6354fb281f70a39435494c420f42d19168
Ebuka Hills <flyg101@gmail.com>
2017-01-31 21:36:03 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Firebase Actions are handled here
+
+// Initialize Firebase
+var config = {
+  apiKey: YOUR_API_KEY_HERE,
+  authDomain: AUTH_DOMAIN_HERE,
+  databaseURL: DATABASE_URL_HERE,
+  storageBucket: STORAGE_BUCKET_HERE,
+  messagingSenderId: MESSAGE_SENDER_ID
+};
+firebase.initializeApp(config);
